5869b262eb096278d4cd1759fda7e6e7dc906d9b
waterloocode <330316259@qq.com>
2018-04-15 16:46:31 +0800

    手机号位数校验

--- src/Rules/Mobile.php
+++ src/Rules/Mobile.php
@@ -42,20 +42,27 @@ class Mobile implements Rule
      */
     public function passes($attribute, $value)
     {
+        if (strlen($value) !== 11) {
+            return false;
+        }
+        if (!is_numeric($value)) {
+            return false;
+        }
         // 中国移动
         $cmcc
-            = '^((13[4-9])|(147)|(15[0-2,7-9])|(17[8])|(18[2-4,7-8]))\\d{8}|(170[5])\\d{7}$';
+            = '((13[4-9])|(147)|(15[0-2,7-9])|(17[8])|(18[2-4,7-8]))[0-9]{8}|(170[5])[0-9]{7}';
         //中国联通
         $uniform
-            = '^((13[0-2])|(145)|(15[5-6])|(17[156])|(18[5,6])|(16[6]))\\d{8}|(170[4,7-9])\\d{7}$';
+            = '((13[0-2])|(145)|(15[5-6])|(17[156])|(18[5,6])|(16[6]))[0-9]{8}|(170[4,7-9])[0-9]{7}';
         //电信
         $telecom
-            = '^((133)|(149)|(153)|(17[3,7])|(18[0,1,9])|(19[9]))\\d{8}|(170[0-2])\\d{7}$';
+            = '((133)|(149)|(153)|(17[3,7])|(18[0,1,9])|(19[9]))[0-9]{8}|(170[0-2])[0-9]{7}';
 
+        $patterns = '/\A%s\z/D';//限定开头和结尾
 
-        return preg_match(sprintf('/%s/', $cmcc), $value)
-            || preg_match(sprintf('/%s/', $uniform), $value)
-            || preg_match(sprintf('/%s/', $telecom), $value);
+        return preg_match(sprintf($patterns, $cmcc), $value)
+            || preg_match(sprintf($patterns, $uniform), $value)
+            || preg_match(sprintf($patterns, $telecom), $value);
     }
 
     /**
